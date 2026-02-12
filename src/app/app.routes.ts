f34f8aa4505fc35b01299bc868a073ba7c718e36
Mick-dud <mba.arevalosabando@gmail.com>
2026-02-12 09:59:43 -0500

    cambio y mejoras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { Acerca } from './features/acerca/acerca';
+import { Consultas } from './features/consultas/consultas';
+import { Pacientes } from './features/pacientes/pacientes';
+import { Contacto } from './features/contacto/contacto';
 
 export const routes: Routes = [
     //1. Ruta inicial
-    { path: 'home', component: Home },
+    { path: '', component: Home },
     //2.Rutas de Navegacion
-    //{ path: 'acerca', component: Acerca },    
-    //{ path: 'consultas', component: Consultas },
-    //{ path: 'mascotas', component: Mascotas },
-    //{ path: 'usuarios', component: Usuarios },
-    
+    { path: 'acerca', component: Acerca },
+    { path: 'consultas', component: Consultas },
+    { path: 'mascotas', component: Pacientes },
+    { path: 'usuarios', component: Contacto },
+
 ];
---
+++ src/app/features/acerca/acerca.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Historia } from "../../shared/historia/historia";
+
+@Component({
+  selector: 'app-acerca',
+  imports: [Historia],
+  templateUrl: './acerca.html',
+  styleUrl: './acerca.css',
+})
+export class Acerca {
+
+}
